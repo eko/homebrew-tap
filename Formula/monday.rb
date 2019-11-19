154c113e34151eb9eadd3974d2115cba481ef992
Vincent Composieux <vincent@composieux.fr>
2019-11-19 13:12:49 +0100

    Added macOS Catalina bottle to monday formula

--- Formula/monday.rb
+++ Formula/monday.rb
@@ -6,6 +6,13 @@ class Monday < Formula
       :revision => "904ea13f32a620146b15d01a15790edfce3c1f3c"
   head "https://github.com/eko/monday.git"
 
+  bottle do
+    root_url "https://github.com/eko/monday/releases/download/v1.0.10/"
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "a753365274ecc908ce8f371ae84c439165af79af32711239551fd1e87eff88a0" => :catalina
+  end
+
   depends_on "go" => :build
 
   def install
