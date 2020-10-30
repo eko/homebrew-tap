8255239242bfc4e43411ca66f15bae8d38d78c98
Vincent Composieux <vincent@composieux.fr>
2020-10-30 13:01:07 +0100

    Monday formula: bump to new release v2.0.0

--- Formula/monday.rb
+++ Formula/monday.rb
@@ -2,14 +2,14 @@ class Monday < Formula
   desc "A dev tool for microservice developers to run local applications and/or forward others from/to Kubernetes SSH or TCP"
   homepage "https://github.com/eko/monday"
   url "https://github.com/eko/monday.git",
-      :tag      => "v1.0.12",
-      :revision => "fa84095a9f61b2736b74b400087d6615f75cf23c"
+      :tag      => "v2.0.0",
+      :revision => "deefeca0ee24172e4253aa5cd0356a361e7f48b2"
   head "https://github.com/eko/monday.git"
 
   bottle do
-    root_url "https://github.com/eko/monday/releases/download/v1.0.12"
+    root_url "https://github.com/eko/monday/releases/download/v2.0.0"
     cellar :any_skip_relocation
-    sha256 "2087c462d831c94bde39dc6d85f7e591f8b7ca2a7cbb833ffbcf7b3630e12d0e" => :catalina
+    sha256 "f035a45ab652b384779ddac4bf9c70d2479c8e0fef3620be881380081bf7e218" => :catalina
   end
 
   depends_on "go" => :build
