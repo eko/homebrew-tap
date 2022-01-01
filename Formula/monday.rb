bc2ecbca50b03c1991c10295453151989cc0d344
eko <github@composieux.fr>
2022-01-01 16:31:19 +0000

    "Brew formula update for monday version v2.1.1"

--- Formula/monday.rb
+++ Formula/monday.rb
@@ -6,27 +6,27 @@ class Monday < Formula
   desc "⚡️ a dev tool for microservice developers to run local applications and/or f"
   homepage "https://github.com/eko/monday"
   url "https://github.com/eko/monday/archive/v2.1.1.tar.gz"
-  sha256 "40d73df17371dbe5b1508f29a38159b59d9cc678747f86d34a2362a1ea05e42b"
+  sha256 "95b52baa76648ea755781cd6a2f875c6477e936dc2b3b6861cdb67c428870135"
   license "MIT"
 
   def install
     ENV["GOPATH"] = buildpath
 
-dir = buildpath/"src/github.com/eko/monday"
-dir.install buildpath.children - [buildpath/".brew_home"]
+  dir = buildpath/"src/github.com/eko/monday"
+  dir.install buildpath.children - [buildpath/".brew_home"]
 
-cd dir do
-  system "make build-binary"
-  bin.install "monday"
+  cd dir do
+    system "make build-binary"
+    bin.install "monday"
 
-  output = Utils.popen_read("#{bin}/monday completion bash")
-  (bash_completion/"monday").write output
+    output = Utils.popen_read("#{bin}/monday completion bash")
+    (bash_completion/"monday").write output
 
-  output = Utils.popen_read("#{bin}/monday completion zsh")
-  (zsh_completion/"_monday").write output
+    output = Utils.popen_read("#{bin}/monday completion zsh")
+    (zsh_completion/"_monday").write output
 
-  prefix.install_metafiles
-end
+    prefix.install_metafiles
+  end
   end
 
   test do
